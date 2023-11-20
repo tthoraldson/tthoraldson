5242c394c8ea1f6d5c66490d57e873ea18e2c5f8
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2023-11-19 19:29:58 -0600

    another try

--- sketch.js
+++ sketch.js
@@ -1,22 +1,15 @@
-let pg;
-
 function setup() {
-    // Create an off-screen graphics buffer
-    pg = createGraphics(400, 400);
+    let pg = createGraphics(400, 400);
 
-    // Draw something on the buffer
+    // Draw on the off-screen graphics buffer
     pg.background(100);
     pg.fill(255);
     pg.noStroke();
     pg.ellipse(pg.width / 2, pg.height / 2, 200, 200);
 
-    // Convert the buffer to an image
-    let imageElement = createImg(pg.elt.toDataURL());
-
-    // Replace the entire body content with the image
-    document.body.innerHTML = '';
-    imageElement.parent(document.body);
-
-    // Remove the default canvas created by p5.js
+    // No need to create a canvas
     noCanvas();
+    
+    // Redirect to the image data URL
+    window.location.href = pg.elt.toDataURL();
 }
