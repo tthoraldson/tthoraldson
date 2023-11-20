e96f6ec14092572edb56ef85ecb01cd9381c7586
Theresa Thoraldson <theresa.thoraldson@outlook.com>
2023-11-19 19:24:32 -0600

    another attempt

--- sketch.js
+++ sketch.js
@@ -1,9 +1,22 @@
-function setup() {
-    createCanvas(400, 400);
-}
+let pg;
 
-function draw() {
-    background(220);
-    fill(random(255), random(255), random(255));
-    ellipse(width / 2, height / 2, 50, 50);
+function setup() {
+    // Create an off-screen graphics buffer
+    pg = createGraphics(400, 400);
+
+    // Draw something on the buffer
+    pg.background(100);
+    pg.fill(255);
+    pg.noStroke();
+    pg.ellipse(pg.width / 2, pg.height / 2, 200, 200);
+
+    // Convert the buffer to an image
+    let imageElement = createImg(pg.elt.toDataURL());
+
+    // Replace the entire body content with the image
+    document.body.innerHTML = '';
+    imageElement.parent(document.body);
+
+    // Remove the default canvas created by p5.js
+    noCanvas();
 }
